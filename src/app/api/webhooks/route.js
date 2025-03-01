0fc946b23a184d21b9ce9a3d5f292f87f1e57a5c
tawkeer1 <towqeerahmad111@gmail.com>
2025-03-01 14:11:03 +0530

    changed webhook and middleware code a little bit

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -1,7 +1,7 @@
-import { Webhook } from "svix";
-import { headers } from "next/headers";
-import { createOrUpdateUser, deleteUser } from "@/lib/actions/user";
-import { clerkClient } from "@clerk/nextjs/server";
+import { Webhook } from 'svix';
+import { headers } from 'next/headers';
+import { createOrUpdateUser, deleteUser } from '@/lib/actions/user';
+import { clerkClient } from '@clerk/nextjs/server';
 
 export async function POST(req) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
@@ -9,22 +9,21 @@ export async function POST(req) {
 
   if (!WEBHOOK_SECRET) {
     throw new Error(
-      "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
+      'Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local'
     );
   }
 
   // Get the headers
-  const svix_id = req.headers.get("svix-id");
-  const svix_timestamp = req.headers.get("svix-timestamp");
-  const svix_signature = req.headers.get("svix-signature");
+  const headerPayload = headers();
+  const svix_id = headerPayload.get('svix-id');
+  const svix_timestamp = headerPayload.get('svix-timestamp');
+  const svix_signature = headerPayload.get('svix-signature');
 
+  // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
-    console.error("Missing Svix headers", {
-      svix_id,
-      svix_timestamp,
-      svix_signature,
+    return new Response('Error occured -- no svix headers', {
+      status: 400,
     });
-    return new Response("Error occurred -- no svix headers", { status: 400 });
   }
 
   // Get the body
@@ -39,13 +38,13 @@ export async function POST(req) {
   // Verify the payload with the headers
   try {
     evt = wh.verify(body, {
-      "svix-id": svix_id,
-      "svix-timestamp": svix_timestamp,
-      "svix-signature": svix_signature,
+      'svix-id': svix_id,
+      'svix-timestamp': svix_timestamp,
+      'svix-signature': svix_signature,
     });
   } catch (err) {
-    console.error("Error verifying webhook:", err);
-    return new Response("Error occured", {
+    console.error('Error verifying webhook:', err);
+    return new Response('Error occured', {
       status: 400,
     });
   }
@@ -55,9 +54,9 @@ export async function POST(req) {
   const { id } = evt?.data;
   const eventType = evt?.type;
   console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
-  console.log("Webhook body:", body);
+  console.log('Webhook body:', body);
 
-  if (eventType === "user.created" || eventType === "user.updated") {
+  if (eventType === 'user.created' || eventType === 'user.updated') {
     const { id, first_name, last_name, image_url, email_addresses, username } =
       evt?.data;
     try {
@@ -69,7 +68,7 @@ export async function POST(req) {
         email_addresses,
         username
       );
-      if (user && eventType === "user.created") {
+      if (user && eventType === 'user.created') {
         try {
           await clerkClient.users.updateUserMetadata(id, {
             publicMetadata: {
@@ -78,28 +77,28 @@ export async function POST(req) {
             },
           });
         } catch (error) {
-          console.log("Error updating user metadata:", error);
+          console.log('Error updating user metadata:', error);
         }
       }
     } catch (error) {
-      console.log("Error creating or updating user:", error);
-      return new Response("Error occured", {
+      console.log('Error creating or updating user:', error);
+      return new Response('Error occured', {
         status: 400,
       });
     }
   }
 
-  if (eventType === "user.deleted") {
+  if (eventType === 'user.deleted') {
     const { id } = evt?.data;
     try {
       await deleteUser(id);
     } catch (error) {
-      console.log("Error deleting user:", error);
-      return new Response("Error occured", {
+      console.log('Error deleting user:', error);
+      return new Response('Error occured', {
         status: 400,
       });
     }
   }
 
-  return new Response("", { status: 200 });
+  return new Response('', { status: 200 });
 }
