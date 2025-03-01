c0d75e96d6c835febd4adc17cbfc8519bff05bda
tawkeer1 <towqeerahmad111@gmail.com>
2025-03-01 22:42:50 +0530

    Changed mongodb urlparser

--- src/lib/mongodb/mongoose.js
+++ src/lib/mongodb/mongoose.js
@@ -11,8 +11,8 @@ export const connect = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
       dbName: 'next-blog',
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
     });
     console.log('Connected to MongoDB');
     initialized = true;
