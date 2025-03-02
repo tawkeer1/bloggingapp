5e59d96104dd8b57a5bf5fef24030463298eadb2
tawkeer1 <towqeerahmad111@gmail.com>
2025-03-02 08:12:49 +0530

    Add another post button

--- src/app/post/[slug]/page.jsx
+++ src/app/post/[slug]/page.jsx
@@ -1,58 +1,65 @@
-import CallToAction from '@/app/components/CallToAction';
-import RecentPosts from '@/app/components/RecentPosts';
-import { Button } from 'flowbite-react';
-import Link from 'next/link';
+
+import CallToAction from "@/app/components/CallToAction";
+import RecentPosts from "@/app/components/RecentPosts";
+import { useUser } from "@clerk/nextjs";
+import { Button } from "flowbite-react";
+import Link from "next/link";
 export default async function PostPage({ params }) {
   let post = null;
   try {
-    const result = await fetch(process.env.URL + '/api/post/get', {
-      method: 'POST',
+    const result = await fetch(process.env.URL + "/api/post/get", {
+      method: "POST",
       body: JSON.stringify({ slug: params.slug }),
-      cache: 'no-store',
+      cache: "no-store",
     });
     const data = await result.json();
     post = data.posts[0];
   } catch (error) {
-    post = { title: 'Failed to load post' };
+    post = { title: "Failed to load post" };
   }
-  if (!post || !post.title === 'Failed to load post') {
+  if (!post || !post.title === "Failed to load post") {
     return (
-      <main className='p-3 flex flex-col max-w-6xl mx-auto min-h-screen'>
-        <h2 className='text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl'>
+      <main className="p-3 flex flex-col max-w-6xl mx-auto min-h-screen">
+        <h2 className="text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl">
           Post not found
         </h2>
       </main>
     );
   }
   return (
-    <main className='p-3 flex flex-col max-w-6xl mx-auto min-h-screen'>
-      <h1 className='text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl'>
+    <main className="p-3 flex flex-col max-w-6xl mx-auto min-h-screen">
+      <h1 className="text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl">
         {post && post.title}
       </h1>
       <Link
         href={`/search?category=${post && post.category}`}
-        className='self-center mt-5'
+        className="self-center mt-5"
       >
-        <Button color='gray' pill size='xs'>
+        <Button color="gray" pill size="xs">
           {post && post.category}
         </Button>
       </Link>
       <img
         src={post && post.image}
         alt={post && post.title}
-        className='mt-10 p-3 max-h-[600px] w-full object-cover'
+        className="mt-10 p-3 max-h-[600px] w-full object-cover"
       />
-      <div className='flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs'>
+      <div className="flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs">
         <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
-        <span className='italic'>
+        <span className="italic">
           {post && (post?.content?.length / 1000).toFixed(0)} mins read
         </span>
       </div>
       <div
-        className='p-3 max-w-2xl mx-auto w-full post-content'
+        className="p-3 max-w-2xl mx-auto w-full post-content"
         dangerouslySetInnerHTML={{ __html: post?.content }}
       ></div>
-      <div className='max-w-4xl mx-auto w-full'>
+      
+        <Link href="/dashboard/create-post" className="text-center flex justify-center items-center font-semibold ">
+        <p className="border border-gray-500 p-1 rounded-md mb-2 bg-gradient-to-r from-blue-500 via-cyan-500 to-teal-500">Create another post</p>
+        </Link>
+      
+      <div className="max-w-4xl mx-auto w-full">
         <CallToAction />
       </div>
       <RecentPosts limit={3} />
