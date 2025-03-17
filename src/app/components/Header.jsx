f30d9313bed825f713f4252abc1ba4cde15f152b
tawkeer1 <towqeerahmad111@gmail.com>
2025-03-17 23:50:49 +0530

    Swapped dark and light icons

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -63,7 +63,7 @@ export default function Header() {
           pill
           onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
         >
-          {theme === 'light' ? <FaSun /> : <FaMoon />}
+          {theme === 'light' ? <FaMoon /> : <FaSun /> }
         </Button>
         <SignedIn>
           <UserButton
