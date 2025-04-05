9b0ed871252c2a85412498b754c9c321040d6f02
tawkeer1 <towqeerahmad111@gmail.com>
2025-04-05 18:33:11 +0530

    sliced fewer characters of content in postCard

--- src/app/components/PostCard.jsx
+++ src/app/components/PostCard.jsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 export default function PostCard({ post }) {
   const plainText = post.content.replace(/<\/?[^>]+(>|$)/g, "");
-  const slicedText = plainText.slice(0, 100);
+  const slicedText = plainText.slice(0, 50);
   const slicedTitle = post.title.slice(0, 35);
   return (
     <div className="group relative w-full border border-gray-400 hover:border-2 h-[400px] overflow-hidden rounded-lg sm:w-[430px] transition-all">
@@ -9,13 +9,13 @@ export default function PostCard({ post }) {
         <img
           src={post.image}
           alt="post cover"
-          className="h-[260px] w-full object-cover group-hover:h-[200px] transition-all duration-300 z-20"
+          className="h-[230px] w-full object-cover group-hover:h-[200px] transition-all duration-300 z-20"
         />
       </Link>
       <div className="p-3 flex flex-col gap-2">
         <p className="text-lg font-semibold line-clamp-2">{slicedTitle} <span className="font-bold text-lg ">{" "}.....</span> </p>
         <span className="italic text-sm">{post.category}</span>
-        <span className="italic text-sm">{slicedText} <span className="font-bold text-lg ">{" "}.....</span> </span>
+        <span className="italic text-sm p-2 mb-2">{slicedText} <span className="font-bold text-lg ">{" "}.....</span> </span>
         <Link
           href={`/post/${post.slug}`}
           className="z-10 group-hover:bottom-0 absolute bottom-[-200px] left-0 right-0 border border-gray-400 text-gray-500 hover:bg-gray-500 hover:text-white transition-all duration-300 text-center py-2 rounded-md !rounded-tl-none m-2"
