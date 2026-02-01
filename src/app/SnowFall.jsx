496837646a9405bd48bfe1ea1a9d28216cb9840f
tawkeer1 <towqeerahmad111@gmail.com>
2026-02-01 22:09:08 +0530

    s

--- src/app/SnowFall.jsx
+++ src/app/SnowFall.jsx
@@ -3,13 +3,20 @@ import React from 'react'
 import Snowfall from 'react-snowfall'
 
 const SnowFall = () => {
+  let date = new Date();
+  let month = date.getMonth() + 1; // getMonth() returns 0-11
+
+  if (month !== 12 && month !== 1 && month !== 2) {
+    return null; // Do not render snowfall outside of December, January, February
+  }
+  
   return (
       <Snowfall
         style={{
           position: "fixed",
           width: "100vw",
           height: "100vh",
-          zIndex: 500,
+          zIndex: 100,
         }}
       />
   )
